d98f6ecece5fd64a8ba04a995949a816e8e99aef
T D <tjarkdreyer@gmx.de>
2024-07-20 00:46:02 +0200

    PlayerMovement

--- GameJam202407/Assets/Scripts/C_PlayerMovement.cs
+++ GameJam202407/Assets/Scripts/C_PlayerMovement.cs
@@ -2,36 +2,116 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCam : MonoBehaviour
+public class C_PlayerMovement : MonoBehaviour
 {
-    public float sensX;
-    public float sensY;
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpforce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatisGround;
+    bool grounded;
 
     public Transform orientation;
 
-    float xRotation;
-    float yRotation;
+    float horizontalInput;
+    float verticalInput;
 
-    private void Start()    
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
     }
 
     private void Update()
     {
-        // get mouse Input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        //Ground Check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatisGround);
+        
+        MyInput();
+        SpeedControl();
 
-        yRotation += mouseX;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        //Rotate Cam And Orientation
-        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
-        orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        //Handle Drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
     }
 
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // when To jump
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump),jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        // Calculate Movement Direction
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        //on Ground
+        if(grounded)
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+   
+        //in Air    
+        else if(!grounded)
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVal = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        //Limit Velocity if needed
+        if(flatVal.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVal.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+
+    private void Jump()
+    {
+        // Reset Y Velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
 }
