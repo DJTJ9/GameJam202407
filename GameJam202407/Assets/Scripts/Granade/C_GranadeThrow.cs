1008845ea859cd9a0b5f200659cca145d7748526
T D <TjarkDreyer@gmx.de>
2024-07-21 03:03:59 +0200

    TargetDetection, PillarDestruction

--- GameJam202407/Assets/Scripts/Granade/C_GranadeThrow.cs
+++ GameJam202407/Assets/Scripts/Granade/C_GranadeThrow.cs
@@ -7,7 +7,9 @@ public class C_GranadeThrow : MonoBehaviour
 {
     public C_GrenadeData GrenadeData;
     public GameObject parentReference;
+    [SerializeField]
     public LayerMask HitMask;
+    public float ExplosionForce = 700f;
     Rigidbody rb;
 
     // Start is called before the first frame update
@@ -20,29 +22,46 @@ public class C_GranadeThrow : MonoBehaviour
     private IEnumerator Selfdestruct()
     {
         float passedTime = 0;
-        while(GrenadeData.ExplosionDelay > passedTime)
+        while (GrenadeData.ExplosionDelay > passedTime)
         {
             passedTime += Time.deltaTime;
             yield return null;
         }
 
-        OverlappTrace();
+        Explode();
 
         Destroy(parentReference);
     }
     void ThrowObject()
     {
-       rb.AddForce(GrenadeData.ThrowStrenght * transform.forward, ForceMode.Impulse);
+        rb.AddForce(GrenadeData.ThrowStrenght * transform.forward, ForceMode.Impulse);
     }
 
-    void OverlappTrace()
+    void Explode()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, GrenadeData.ExplosionRadius,HitMask);
-   
-    foreach(Collider collider in colliders)
+        // Sucht zuerst nach Pillars in der Range der Granante und zerstört diese
+        Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, GrenadeData.ExplosionRadius);
+
+        foreach (Collider nearbyObject in collidersToDestroy)
         {
-        
-        
+            Pillar pillar = nearbyObject.GetComponent<Pillar>();
+            if (pillar != null)
+            {
+                pillar.Destroy();
+            }           
+
+        }
+        // Sucht dann nach Rigidbodies, die auf den Trümmerteilen liegen, um auf diese eine Kraft auszuwirken
+        // und sie expolsionsartig wegfleigen zu lassen
+        Collider[] collidersToMove = Physics.OverlapSphere(transform.position, GrenadeData.ExplosionRadius/*, HitMask*/);
+
+        foreach (Collider nearbyFragments in collidersToMove)
+        {
+            Rigidbody rb = nearbyFragments.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(ExplosionForce, transform.position, GrenadeData.ExplosionRadius);
+            }
         }
     }
 }
